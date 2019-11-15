f76c2bd296757243be9c590f86c927237bb2bdb9
Jorge Fernández Oré <jorgearmando_JAF@outlook.com>
2019-11-14 20:31:13 -0500

    Initial Commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Jorge Fernández Oré on 11/14/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
